df39208f969f0fe8f87c8cc794e157a252c1c063
SharmaYashasvi <202011079@diu.iiitvadodara.ac.in>
2023-04-09 16:53:27 +0530

    jai shri ram

--- coursebundlerserver/config/database.js
+++ coursebundlerserver/config/database.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 
+// connexting database
 export const connectDB = async () =>{
     const {connection } = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDb per jai shri ram on ${connection.host}`);
