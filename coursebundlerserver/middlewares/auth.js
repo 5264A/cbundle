04c4c0173abef17d6329b75e20761d3bce3c1b88
SharmaYashasvi <202011079@diu.iiitvadodara.ac.in>
2023-04-12 00:10:05 +0530

    jai shri ram

--- coursebundlerserver/middlewares/auth.js
+++ coursebundlerserver/middlewares/auth.js
@@ -6,7 +6,7 @@ import { User } from "../models/User.js";
 export const isAuthenticated = catchAsyncError(async (req, res, next) => {
   const { token } = req.cookies;
 
-  if (!token) return next(new ErrorHandler("Not Logged In", 401));
+  if (!token) return next(new ErrorHandler("Please login to access this", 401));
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
@@ -14,3 +14,18 @@ export const isAuthenticated = catchAsyncError(async (req, res, next) => {
 
   next();
 });
+
+export const authorizeAdmin = (req, res, next) => {
+  if (req.user.role !== "admin")
+    return next(
+      new ErrorHandler(
+        `${req.user.role} is not allowed to access this resource`,
+        403
+      )
+    );
+
+  next();
+};
+
+
+// 403 server denai
--- coursebundlerserver/routes/courseRoutes.js
+++ coursebundlerserver/routes/courseRoutes.js
@@ -1,12 +1,32 @@
-import express from 'express';
-import { createCourse, getAllCourse } from '../controllers/courseController.js';
+import express from "express";
+import {
+  addLecture,
+  createCourse,
+  deleteCourse,
+  deleteLecture,
+  getAllCourse,
+  getCourseLectures,
+} from "../controllers/courseController.js";
+import singleUpload from "../middlewares/multer.js";
+import { authorizeAdmin, isAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
+
 // get all courses without lectures
 router.route("/courses").get(getAllCourse);
-// create courses -- admin
-router.route("/createcourse").post(createCourse);
 
-// add lectures , delete Course , get course details
+// create courses -- admin
+router
+  .route("/createcourse")
+  .post(isAuthenticated, authorizeAdmin, singleUpload, createCourse);
+
+// add lectures , get course lectures , delete course
+router
+  .route("/course/:id")
+  .get(isAuthenticated, getCourseLectures)
+  .post(isAuthenticated, authorizeAdmin, singleUpload, addLecture)
+  .delete(isAuthenticated, authorizeAdmin, deleteCourse);
 
 // delete lectures
+router.route("/lecture").delete(isAuthenticated, authorizeAdmin, deleteLecture);
+
 export default router;
